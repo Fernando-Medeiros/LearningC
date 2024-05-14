069797e26aa72364e8bb58bbb3b633aebd045566
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-14 09:22:48 -0300

    feat: examples > FileSystem (file-system-library)

--- Examples/src/FileSystem.cpp
+++ Examples/src/FileSystem.cpp
@@ -1,14 +1,42 @@
 #include <filesystem>
+#include <format>
 #include <iostream>
 #include <string>
 
-static bool exists(const std::string& path) {
-	return std::filesystem::exists(path);
-}
+using
+std::cout,
+std::string,
+std::format,
+std::filesystem::path,
+std::filesystem::exists,
+std::filesystem::absolute,
+std::filesystem::canonical,
+std::filesystem::current_path,
+std::filesystem::directory_iterator;
 
+// I/O
 static void FileSystem() {
-	std::string home = "HOME"; // Depreacated getenv("HOME");
-	home = home + "/";
+	auto canonicalPath{ canonical(path(".")) };
+	auto absolutePath{ absolute(canonicalPath) };
+	auto currentPath{ current_path() };
+	auto srcPath{ path(canonicalPath) /= "src" };
+	auto rawPath{ path(canonicalPath) /= "raw" };
 
-	std::cout << (exists(home + "Downloads") ? "Yes" : "Not") << "\n";
+	cout << "canonical: " << canonicalPath << "\n";
+	cout << "absolute : " << absolutePath << "\n";
+	cout << "current  : " << currentPath << "\n";
+	cout << "src	  : " << srcPath << "\n";
+	cout << "raw	  : " << rawPath << "\n";
+
+	auto coutFiles = [](path& p) {
+		cout << ":: " << p << "\n";
+		for (auto& entry : directory_iterator(p)) {
+			cout << entry.path().filename() << "\n";
+		}};
+
+	coutFiles(srcPath);
+	coutFiles(rawPath);
+	coutFiles(currentPath);
+
+	cout << "Text.txt ?: " << exists(rawPath.concat("\\Text.txt")) << "\n";
 }
